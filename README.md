ba2875c29e8690aa1c90b2822cb574497b88c9a2
rankun203 <rankun203@gmail.com>
2013-08-01 19:44:39 +0800

    基本获取功能完成。

--- README.md
+++ README.md
@@ -1,20 +1,37 @@
 描述：网页词典内容截取器
 
-用法：FetchWord -m <outputType> -w <word>|-s <file> -d <dir>|-f <file>
+用法：FetchWord -m <outputType> -w <word>|-s <file> -d <dir>|-f <file> -t <outputStyle>
 
-  -w     指定单词
-  -d     指定路径存放抓取结果
-  -s     指定单词列表文件，程序扫描该文件读取所有单词并抓取其释义
-  -m     指定输出类型
-          默认是
-           print
-          可能的值有
-           print    将结果打印在屏幕上
-           folder   将结果存入指定的文件夹中
-                     参数列表中必须包含-d选项并指明存储的位置
-           file     将结果存入单个文件中
-                     参数列表中必须包含-f选项并指明存储文件的位置
-  -h     显示本帮助
+  -iw     指定单词
+  -is     指定单词列表文件，程序扫描该文件读取所有单词并抓取其释义
+  -od     指定一个文件夹存放抓取结果
+  -of     指定一个文件存放抓取结果
+  -m      指定输出类型
+           默认是
+            print
+           可能的值有
+            print    将结果打印在屏幕上
+            folder   将结果存入指定的文件夹中
+                      参数列表中必须包含-d选项并指明存储的位置
+            file     将结果存入单个文件中
+                      参数列表中必须包含-f选项并指明存储文件的位置
+           通常情况下，不指定输出位置，程序将直接打印，否则输出结果到指定位置
+            只要输出类型不特殊注明为其它输出方式：
+             如果使用-of指定了输出文件，则输出结果到单独的文件
+             如果使用-od指定了输出文件夹，则输出结果到指定的文件夹
+  -c      指定词典
+           默认值是
+            youdaocollins
+           可能的值有
+            youdaocollins   从有道词典网站获取Collins词典的释义
+  -s      指定输出内容的格式
+           默认是
+            plain
+           可能的值有
+            plain           纯文本方式显示内容
+            singleStyle     将样式直接写入HTML中
+			separateStyle   将样式分布在HTML页面之外的css文件中
+  -h      显示本帮助
 
 用法示例：
 
@@ -22,15 +39,27 @@
 java -cp .:/home/mindfine/Java/lib/jsoup/jsoup-1.7.2.jar com.mindfine.youdaodict.FetchWord -h
 
 2.获取 hello 的释义并将结果打印出来
-java -cp .:/home/mindfine/Java/lib/jsoup/jsoup-1.7.2.jar com.mindfine.youdaodict.FetchWord -w hello -d /home/mindfine/dict
+java -cp .:/home/mindfine/Java/lib/jsoup/jsoup-1.7.2.jar com.mindfine.youdaodict.FetchWord -iw hello -m print
 
 3.获取 hello 的释义并将结果存入 "/home/mindfine/dict/" 文件夹中
+java -cp .:/home/mindfine/Java/lib/jsoup/jsoup-1.7.2.jar com.mindfine.youdaodict.FetchWord -iw hello -od /home/mindfine/dict/
 
 4.获取 hello 的释义并将结果存入 "/home/mindfine/dict.txt" 文件中
+java -cp .:/home/mindfine/Java/lib/jsoup/jsoup-1.7.2.jar com.mindfine.youdaodict.FetchWord -iw hello -of /home/mindfine/dict.txt
 
-5.解析 "/home/mindfine/dic.txt" 中的所有单词，并将释义存入 "/home/mindfine/dict/" 文件夹中
+5.解析 "/home/mindfine/words.txt" 中的所有单词，并将释义存入 "/home/mindfine/dict/" 文件夹中
+java -cp .:/home/mindfine/Java/lib/jsoup/jsoup-1.7.2.jar com.mindfine.youdaodict.FetchWord -is /home/mindfine/words.txt -od /home/mindfine/dict/
 
-6.解析 "/home/mindfine/dic.txt" 中的所有单词，并将释义写入 "/home/mindfine/dict.txt" 文件中
+6.解析 "/home/mindfine/words.txt" 中的所有单词，并将释义写入 "/home/mindfine/dict.txt" 文件中
+java -cp .:/home/mindfine/Java/lib/jsoup/jsoup-1.7.2.jar com.mindfine.youdaodict.FetchWord -is /home/mindfine/words.txt -of /home/mindfine/dict.txt
+
+
+源单词文件的格式为：
+hello
+many
+tiny
+funny
+...
 
 请在项目主页报告程序的错误，欢迎任何形式的复制和转发
 项目主页：https://github.com/rankun203/YoudaoDict
--- YoudaoDict/src/com/mindfine/youdaodict/fetcher/YoudaoCollinsFetcher.java
+++ YoudaoDict/src/com/mindfine/youdaodict/fetcher/YoudaoCollinsFetcher.java
@@ -1,48 +1,103 @@
 package com.mindfine.youdaodict.fetcher;
 
+import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 /**
- * YoudaoCollinsFetcher可以从有道词典拦截Collins的释义
- * 
+ * 从有道词典截取Collins的释义
  * @author mindfine
  */
 public class YoudaoCollinsFetcher implements Fetcher {
-	public static String fetchFromURL = "http://dict.youdao.com/search";
-
+	public String fetchFromURL = "http://dict.youdao.com/search";
+	public Fetcher.StyleType styleType;
+	
 	@Override
 	public String getResFromWord(String word) {
 		return jsoupFetcher(word);
 	}
 
 	
+	/**
+	 * @param word 要查询的单词
+	 * @return 返回使用Jsoup取得的释义数据
+	 */
 	public String jsoupFetcher(String word){
 		String rtn = "";
 		String queryUrl = fetchFromURL + "?q=" + word;
 		try {
 			URL qu = new URL(queryUrl);
 			Document doc = Jsoup.parse(qu, 16000);
-			Element collinsRes = doc.getElementById("collinsResult");
-			rtn = collinsRes.html();
+			if(styleType == StyleType.plain) {
+				rtn = getPlainExplain(doc);
+			} else {
+				Element collinsRes = doc.getElementById("collinsResult");
+				rtn = collinsRes.html();				
+			}
 		} catch (MalformedURLException e) {
 			System.err.println("ERROR, queryUrl is invalid.");
 			e.printStackTrace();
 		} catch (IOException e) {
-			System.err.println("open connection failed.");
+			System.err.println("open connection failed.\r\nNetwork may not accessible.");
 			e.printStackTrace();
 		} catch (NullPointerException e) {
 			return null;
 		}
 		return rtn;
 	}
+	private String getPlainExplain(Document doc) {
+		StringBuilder s = new StringBuilder();
+		Element ctnEle = doc.getElementById("collinsResult").child(0).child(0).child(0).child(0);
+		Element headEle = ctnEle.child(0);
+		Element meansEle = ctnEle.child(1);
+		s.append("http://dict.youdao.com------------------------------\r\n");
+		s.append("[" + headEle.select("span.title").text() + "] ");
+		s.append(" " + headEle.select("em.phonetic").text());
+		String star = headEle.select("span.star").attr("class");
+		int starNo = 0;
+		if(star != null){
+			if(star.indexOf("1") != -1)			starNo = 1;
+			else if (star.indexOf("2") != -1)	starNo = 2;
+			else if (star.indexOf("3") != -1)	starNo = 3;
+			else if (star.indexOf("4") != -1)	starNo = 4;
+			else if (star.indexOf("5") != -1)	starNo = 5;
+			s.append(" ");
+			for(int i = 0; i < starNo; i++){
+				s.append("* ");
+			}
+		}
+		Elements additionalPtn = headEle.select("span.additional");
+		if(additionalPtn.size() > 0) {
+			s.append(additionalPtn.get(0).text() + " ");
+		}
+		s.append("|" + headEle.select("span.rank").text() + "| ");
+		if(additionalPtn.size() > 1) {
+			s.append(additionalPtn.get(1).text() + " ");
+		}
+		
+		s.append(headEle.select("span.title").html());
+		s.append(headEle.select("span.title").html());
+		s.append(headEle.select("span.title").html());
+		s.append(headEle.select("span.title").html());
+		s.append(headEle.select("span.title").html());
+		s.append(headEle.select("span.title").html());
+		s.append(headEle.select("span.title").html());
+		
+		return new String(s);
+	}
+
 	/**
 	 * @param word 要查询的单词
 	 * @return 根据单词得到的整个页面
@@ -70,5 +125,15 @@ public class YoudaoCollinsFetcher implements Fetcher {
 			e.printStackTrace();
 		}
 		return returnString;		
+	}
+
+	@Override
+	public Fetcher.StyleType getStyleType() {
+		return styleType;
+	}
+
+	@Override
+	public void setStyleType(Fetcher.StyleType styleType) {
+		this.styleType = styleType;
 	}
 }
