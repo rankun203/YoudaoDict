15033e20ab45ac55a521aa810c2f1b607905abd8
rankun203 <rankun203@gmail.com>
2013-08-01 10:24:20 +0800

    初次提交，打印命令行参数

---
+++ YoudaoDict/src/com/mindfine/youdaodict/FetchWord.java
@@ -0,0 +1,35 @@
+package com.mindfine.youdaodict;
+
+/**
+ * <h3>爬单词<h3><br>
+ * 1.指定单词和爬到的数据存储的位置,示例：java FetchWord -w many -d /home/mindfine/dict/<br>
+ * 2.指定单词数据文件及数据存储位置,示例：java FetchWord -s /home/mindfine/dict.txt -d
+ * /home/mindfine/dict<br>
+ * 
+ * @author mindfine
+ */
+public class FetchWord {
+	private static String sourceWord = "";
+	private static String saveToDic = "";
+	private static String srcWordFile = "";
+
+	// resolve args from commandLine
+	public static void main(String[] args) {
+
+		for (int i = 0; i < args.length; i++) {
+			String arg = args[i];
+			if (arg != null && arg != "") {
+				if (arg.equals("-w")) {
+					sourceWord = args[i + 1];
+				} else if (arg.equals("-s")) {
+					srcWordFile = args[i + 1];
+				} else if (arg.equals("-d")) {
+					saveToDic = args[i + 1];
+				}
+			}
+		}
+
+		System.out.println("sourceWord = " + sourceWord + ", saveToDict= "
+				+ saveToDic + ", srcWordFile= " + srcWordFile);
+	}
+}
